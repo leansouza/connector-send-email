f0a1f85f100d85598c4ec24403f830e03e29fffd
David Callizaya <davidcallizaya@gmail.com>
2018-12-03 15:57:55 -0400

    Change email vue

--- resources/js/connectors/email/send/component.vue
+++ resources/js/connectors/email/send/component.vue
@@ -12,39 +12,5 @@ export default {
       this.$parent.loadInspector('processmaker-communication-email-send', this.node.definition, this);
     },
   },
-  mounted() {
-    this.shape = new decoratedShape();
-    let bounds = this.node.diagram.bounds;
-    this.shape.position(bounds.x, bounds.y);
-    this.shape.resize(bounds.width, bounds.height);
-    this.shape.attr({
-      body: {
-        rx: 8,
-        ry: 8,
-      },
-      label: {
-        text: joint.util.breakText(this.node.definition.get('name'), { width: bounds.width }),
-        fill: 'black',
-      },
-
-      image: { 'xlink:href': require('./icon.svg') }
-    });
-    this.shape.on('change:position', (element, position) => {
-      this.node.diagram.bounds.x = position.x;
-      this.node.diagram.bounds.y = position.y;
-      // This is done so any flows pointing to this task are updated
-      this.$emit(
-        'move',
-        {
-          x: bounds.x,
-          y: bounds.y,
-        },
-        element
-      );
-    });
-    this.shape.addTo(this.graph);
-    this.shape.component = this;
-    this.$parent.nodes[this.id].component = this;
-  },
 };
 </script>
