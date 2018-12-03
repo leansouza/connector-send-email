8e238678ac3403642d64bf137818f2b63ce8bad2
David Callizaya <davidcallizaya@gmail.com>
2018-12-03 14:41:44 -0400

    Fix register node.

--- resources/js/email-connector.js
+++ resources/js/email-connector.js
@@ -2,8 +2,9 @@ import emailSend from './connectors/email/send/index';
 let nodeTypes = [
     emailSend,
 ];
-ProcessMaker.EventBus.$on('modeler-init', function(modeler) {
-    for (var node of nodeTypes) {
-        modeler.registerNode(node, () => node.id);
+window.ProcessMaker.EventBus.$on('modeler-init', ({ registerNode, registerBpmnExtension })  => {
+    /* Register basic node types */
+    for (const node of nodeTypes) {
+        registerNode(node);
     }
 });
