2486bfe22c1abbd20278534f79ad4cae5b1e5782
David Callizaya <davidcallizaya@gmail.com>
2018-12-04 11:58:14 -0400

    Implement seeding of script implementation.

---
+++ src/Seeds/EmailSendSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ProcessMaker\Packages\Connectors\Email\Seeds;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Cache;
+use ProcessMaker\Models\Script;
+
+class EmailSendSeeder extends Seeder
+{
+
+    const IMPLEMENTATION_ID = 'processmaker-communication-email-send';
+
+    /**
+     * Creates or updates the script implementation.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $definition = [
+            'key' => self::IMPLEMENTATION_ID,
+            'title' => 'Email Send',
+            'description' => 'Send an email',
+            'language' => 'PHP',
+            'code' => $this->getCode(),
+        ];
+        $exists = Script::where('key', self::IMPLEMENTATION_ID)->first();
+        if ($exists) {
+            $exists->fill($definition);
+        } else {
+            factory(Script::class)->create($definition);
+        }
+    }
+
+    private function getCode()
+    {
+        return file_get_contents(__DIR__ . '/code/EmailSend.php');
+    }
+}
