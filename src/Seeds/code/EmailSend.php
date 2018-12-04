a2718cfb5b7caea36e9a5e6072d7a2221dbd051a
David Callizaya <davidcallizaya@gmail.com>
2018-12-04 13:27:34 -0400

    Email send implementation

--- src/Seeds/code/EmailSend.php
+++ src/Seeds/code/EmailSend.php
@@ -1,2 +1,11 @@
 <?php
-extract('data');
+$to      = $config['targetName'] . '<' . $config=['email'] . '>';
+$subject = $config['subject'];
+$message = 'hello world';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+
+return [];
