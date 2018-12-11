ec610dc53c969735d1e7dd475ef17dbff9c82b5e
David Callizaya <davidcallizaya@gmail.com>
2018-12-11 10:38:44 -0400

    Add bootstrap style to the email.

--- src/views/layout.blade.php
+++ src/views/layout.blade.php
@@ -3,9 +3,11 @@
     <head>
         <title>{{$subject}}</title>
         <meta charset="utf-8">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
+        <style>{{file_get_contents("https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css")}}</style>
     <body>
         {!! $body !!}
     </body>
