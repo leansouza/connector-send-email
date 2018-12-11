e021aa40ea0f63748c9161f9a77962d7ad940e11
David Callizaya <davidcallizaya@gmail.com>
2018-12-11 11:11:42 -0400

    Register vuetable in the renderer

--- resources/js/ssr/src/App.vue
+++ resources/js/ssr/src/App.vue
@@ -10,7 +10,11 @@ import VueFormRenderer from "@processmaker/vue-form-builder/src/components/vue-f
 import FormText from "@processmaker/vue-form-builder/src/components/renderer/form-text";
 import FormMultiColumn from "@processmaker/vue-form-builder/src/components/renderer/form-multi-column";
 import FormRecordList from "@processmaker/vue-form-builder/src/components/renderer/form-record-list";
+import Vuetable from "vuetable-2/src/components/Vuetable";
+import VuetablePagination from "vuetable-2/src/components/VuetablePagination";
 
+Vue.component('Vuetable', Vuetable);
+Vue.component('VuetablePagination', VuetablePagination);
 Vue.component('VueFormRenderer', VueFormRenderer);
 Vue.component('FormText', FormText);
 Vue.component('FormMultiColumn', FormMultiColumn);
