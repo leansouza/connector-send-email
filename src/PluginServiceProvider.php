265698ce3c7406c863b9489279b9959ae1dc4d84
David Callizaya <davidcallizaya@gmail.com>
2018-12-13 12:10:01 -0400

    Register the seeder instead of calling from the update.
    Now after the package discover, the administrator should execute: php artisan db:seed
    to populate or update the tables.

--- src/PluginServiceProvider.php
+++ src/PluginServiceProvider.php
@@ -33,6 +33,9 @@ class PluginServiceProvider extends ServiceProvider
         //Register email templates
         $this->loadViewsFrom(__DIR__ . '/views', 'email');
 
+        //Register a seeder that will be executed in php artisan db:seed
+        $this->registerSeeder(EmailSendSeeder::class);
+
         // Complete the plugin booting
         $this->completePluginBoot();
     }
@@ -43,8 +46,6 @@ class PluginServiceProvider extends ServiceProvider
      */
     protected function updateVersion()
     {
-        $seed = new EmailSendSeeder;
-        $seed->run();
         Artisan::call('vendor:publish',
             [
             '--tag' => 'bpm-package-email-connector',
