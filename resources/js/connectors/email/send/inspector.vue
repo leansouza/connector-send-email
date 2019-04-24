951053ea9f6b359b01bc6d9e5ccca48c67d1d6b1
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-04-24 16:09:46 -0400

    Allow for translations

--- resources/js/connectors/email/send/inspector.vue
+++ resources/js/connectors/email/send/inspector.vue
@@ -1,72 +1,71 @@
 <template>
     <div>
         <div class="form-group">
-            <label>Email</label>
+            <label>{{ $t('Email') }}</label>
             <input v-model="config.email" class="form-control">
-            <small class="form-text text-muted">Recipient's email address</small>
+            <small class="form-text text-muted">{{ $t('Recipient\'s email address') }}</small>
         </div>
         <div class="form-group">
-            <label>Name</label>
+            <label>{{ $t('Name') }}</label>
             <input v-model="config.targetName" class="form-control">
-            <small class="form-text text-muted">Recipient's name</small>
+            <small class="form-text text-muted">{{ $t('Recipient\'s name') }}</small>
         </div>
         <div class="form-group">
-            <label>Subject</label>
+            <label>{{ $t('Subject') }}</label>
             <input v-model="config.subject" class="form-control">
-            <small class="form-text text-muted">Email subject</small>
+            <small class="form-text text-muted">{{ $t('Email subject') }}</small>
         </div>
-        <modeler-screen-select label="Email body"
-                               helper='What Screen Should Be Used For Sending This Email'
+        <modeler-screen-select :label="$t('Email body')"
+                               :helper="$t('What Screen Should Be Used For Sending This Email')"
                                :params='{type:"EMAIL"}'
-                               v-model='config.screenRef'
-                               ></modeler-screen-select>
+                               v-model='config.screenRef'>
+        </modeler-screen-select>
     </div>
 </template>
 
 
 <script>
-    export default {
-        props: ["value"],
-        data() {
-            return {
-                config: {
-                    email: '',
-                    targetName: '',
-                    subject: '',
-                    screenRef: '',
-                },
-            };
+  export default {
+    props: ["value"],
+    data() {
+      return {
+        config: {
+          email: '',
+          targetName: '',
+          subject: '',
+          screenRef: '',
         },
-        watch: {
-            config: {
-                deep: true,
-                handler() {
-                    this.updateConfig();
-                }
-            },
-            value() {
-                this.loadConfig();
-            },
-        },
-        computed: {
-        },
-        methods: {
-            loadConfig() {
-                const node = this.$parent.$parent.highlightedNode.definition;
-                const config = JSON.parse(_.get(node, 'config'));
-                Object.keys(config).forEach(key => {
-                    Vue.set(this.config, key, config[key]);
-                });
-            },
-            updateConfig() {
-                const node = this.$parent.$parent.highlightedNode.definition;
-                Vue.set(node, 'config', JSON.stringify(this.config));
-            },
-        },
-        mounted() {
-            this.loadConfig();
+      };
+    },
+    watch: {
+      config: {
+        deep: true,
+        handler() {
+          this.updateConfig();
         }
-    };
+      },
+      value() {
+        this.loadConfig();
+      },
+    },
+    computed: {},
+    methods: {
+      loadConfig() {
+        const node = this.$parent.$parent.highlightedNode.definition;
+        const config = JSON.parse(_.get(node, 'config'));
+        Object.keys(config).forEach(key => {
+          Vue.set(this.config, key, config[key]);
+        });
+      },
+      updateConfig() {
+        const node = this.$parent.$parent.highlightedNode.definition;
+        Vue.set(node, 'config', JSON.stringify(this.config));
+      },
+    },
+    mounted() {
+      this.loadConfig();
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
