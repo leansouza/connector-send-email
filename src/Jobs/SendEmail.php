bde33613356da3c91e5dd1fc7b8b322420a4404f
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-05-03 16:22:45 -0400

    Add job SendEmail

---
+++ src/Jobs/SendEmail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace ProcessMaker\Packages\Connectors\Email\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Message;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+
+class SendEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $properties;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param $properties
+     */
+    public function __construct($properties)
+    {
+        $this->properties = $properties;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::send([], [], function (Message $message) {
+            $message->to($this->properties['email'], $this->properties['name'])
+                ->subject($this->properties['subject'])
+                ->setBody(view('email::layout', array_merge($this->properties, ['message' => $message]))->render(), 'text/html');
+        });
+    }
+}
