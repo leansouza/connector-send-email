8d7059c2746c6069af2c779336f21718f2393d07
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-08-12 11:49:45 -0400

    Add support screen definition or plain text

--- src/Controllers/EmailController.php
+++ src/Controllers/EmailController.php
@@ -12,8 +12,6 @@ use ProcessMaker\Models\User;
 use ProcessMaker\Packages\Connectors\Email\Jobs\SendEmail;
 use ProcessMaker\Packages\Connectors\Email\ScreenRenderer;
 
-use Illuminate\Support\Facades\Log;
-
 class EmailController extends Controller
 {
     /**
@@ -26,28 +24,14 @@ class EmailController extends Controller
     {
         $config = $request->input();
 
-        Log::info('Information config ');
-        Log::info(json_encode($config));
-
+        //Validate data
         $users  = !empty($config['groups']) ? $config['groups'] : [];
         $groups  = !empty($config['users']) ? $config['users'] : [];
         $additionalEmails =!empty( $config['addEmails']) ? $config['addEmails'] : [];
-
-        Log::info('Information config ');
-        Log::info(json_encode($config));
+        $type =!empty( $config['type']) ? $config['type'] : 'screen';
 
         //Load data
         $data = json_decode($config['json_data']);
-
-        Log::info('Information groups ');
-        Log::info(json_encode($groups));
-
-        Log::info('Information users ');
-        Log::info(json_encode($users));
-
-        Log::info('Information additionalEmails ');
-        Log::info(json_encode($additionalEmails));
-
 
         //Load mails
         $usersGroups = GroupMember::whereIn('group_id', $groups)->where('member_type', User::class)->pluck('member_id')->toArray();
@@ -62,23 +46,23 @@ class EmailController extends Controller
             $emails[] = $mustache->render($item, $data);
         }
 
-        Log::info('Information emails ');
-        Log::info(json_encode($emails));
-
         //load Body
-        $screen = Screen::find($config['screenRef']);
-        $data = json_decode($config['json_data']);
+        if ($type === 'screen') {
+            //screen definition
+            $screen = Screen::find($config['screenRef']);
+            $data = json_decode($config['json_data']);
 
-        $rendered = ScreenRenderer::render($screen->config, $data);
-        $config['body'] = $rendered;
-        Log::info('Screen rendered');
-        Log::info($rendered);
+            $rendered = ScreenRenderer::render($screen->config, $data);
+            $config['body'] = $rendered;
+        } else {
+            //Plain text
+            $config['body'] = htmlentities($config['textBody'], ENT_QUOTES, 'UTF-8');
+        }
 
         //change mustache
         $config['subject'] = $mustache->render($config['subject'], $data);
         $config['email'] = $emails;
         $config['name'] = $mustache->render($config['name'], $data);
-
 
         //created queue job
         dispatch(new SendEmail($config));
