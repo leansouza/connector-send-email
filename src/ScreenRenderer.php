0974f279c260b284c784a89a2ffb79daa610eb7d
Marco Antonio Nina Mena <marco.antonio.nina@processmaker.com>
2019-08-12 15:12:11 -0400

    remove code test

--- src/ScreenRenderer.php
+++ src/ScreenRenderer.php
@@ -10,7 +10,7 @@ class ScreenRenderer
     public static function render($screen_config, $data)
     {
         $node = (function_exists('env') ? env('NODE_BIN_PATH') : '') ?: exec('which node') ?: '`which node`' ?: 'node';
-        $engine = new Node($node, '/Users/davidcallizaya/Netbeans');
+        $engine = new Node($node, '/tmp');
         $renderer = new Renderer($engine);
         return $renderer
             ->debug()
