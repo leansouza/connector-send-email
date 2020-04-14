180b9a4b81508c41e2c3beef490efc3486996f3d
sanjacornelius <sanja.cornelius@processmaker.com>
2020-04-13 17:52:34 -0700

    Update Webpack to have multiple output configiurations

--- resources/js/ssr/webpack.config.js
+++ resources/js/ssr/webpack.config.js
@@ -2,12 +2,9 @@
 const { VueLoaderPlugin } = require('vue-loader');
 const webpack = require('webpack');
 
-module.exports = {
+var config = {
   mode: 'development',
   target: 'node',
-  entry: [
-    './src/app.js'
-  ],
   module: {
     rules: [
       {
@@ -31,3 +28,44 @@ module.exports = {
     new VueLoaderPlugin(),
   ],
 }
+
+var entryConfig = Object.assign({}, config, {
+  name: 'entry',
+  entry: './entry.js',
+  externals: {
+    canvas: "commonjs canvas"
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'entry.js'
+  }
+});
+
+var mainConfig = Object.assign({}, config, {
+  name: 'main',
+  entry: './src/app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'main.js'
+  }
+});
+
+module.exports = [ entryConfig, mainConfig];
+
+// module.exports = {
+//   mode: 'development',
+//   target: 'node',
+//   entry: [
+//     './entry.js',
+//     './src/app.js'
+//   ],
+//   module: {
+   
+//   },
+//   resolve: {
+//     extensions: ['*', '.js', '.vue', '.json']
+//   },
+//   plugins: [
+//     new VueLoaderPlugin(),
+//   ],
+// }
