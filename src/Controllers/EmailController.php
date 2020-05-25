bd9fbb35bdf865257bb3d0fb63322f8606f2b8ab
David Callizaya <davidcallizaya@gmail.com>
2020-05-25 12:13:09 -0400

    Move email rendering to job

--- src/Controllers/EmailController.php
+++ src/Controllers/EmailController.php
@@ -4,16 +4,8 @@ namespace ProcessMaker\Packages\Connectors\Email\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
 use ProcessMaker\Http\Controllers\Controller;
-use ProcessMaker\Models\GroupMember;
-use ProcessMaker\Models\Screen;
-use ProcessMaker\Models\User;
-use ProcessMaker\Models\Comment;
-use ProcessMaker\Models\ProcessRequest;
 use ProcessMaker\Packages\Connectors\Email\Jobs\SendEmail;
-use ProcessMaker\Packages\Connectors\Email\ScreenRenderer;
 
 class EmailController extends Controller
 {
@@ -27,99 +19,9 @@ class EmailController extends Controller
     {
         $config = $request->input();
 
-        //Load data
-        $data = json_decode($config['json_data'], true);
-
-        //Mustache
-        $mustache = new \Mustache_Engine;
-
-        //Mustache data notification
-        if (isset($data['notification_config'])) {
-            foreach ($data['notification_config'] as $key => $value) {
-                if (is_array($value)) {
-                    $data['notification_config'][$key] = $mustache->render(implode(", ", $value), $data);
-                } else {
-                    $data['notification_config'][$key] = $mustache->render((string) $value, $data);
-                }
-            }
-        }
-
-        //Validate data
-        $groups = !empty($config['groups']) ? $this->getData($config['groups'], $data) : [];
-        $users = !empty($config['users']) ? $this->getData($config['users'], $data) : [];
-        $additionalEmails = !empty($config['addEmails']) ? $this->getData($config['addEmails'], $data) : [];
-        $type =!empty( $config['type']) ? $mustache->render($config['type'], $data) : 'screen';
-
-        //Load mails
-        $usersGroups = GroupMember::whereIn('group_id', $groups)
-            ->where('member_type', User::class)
-            ->pluck('member_id')
-            ->toArray();
-        $users = array_merge($users, $usersGroups);
-        $emails = User::whereIn('id', $users)
-            ->pluck('email')
-            ->toArray();
-
-        //Add additional emails
-        foreach ($additionalEmails as $item) {
-            $item = $mustache->render($item, $data);
-            foreach (explode(",", $item) as $email) {
-                $emails[] = trim($email);
-            }
-        }
-
-        //load Body
-        if ($type === 'screen') {
-            //screen definition
-            $screen = Screen::find($mustache->render($config['screenRef'], $data));
-            $rendered = ScreenRenderer::render($screen->config, $data);
-            $config['body'] = $rendered;
-        } else {
-            //Plain text
-            $config['body'] = htmlentities($mustache->render($config['textBody'], $data), ENT_QUOTES, 'UTF-8');
-        }
-
-        //change mustache
-        $config['subject'] = $mustache->render($config['subject'], $data);
-        $config['email'] = array_filter($emails);
-
         //created queue job
         dispatch(new SendEmail($config));
 
-        if (isset($data['notification_config'])) {
-            Comment::create([
-                'type' => 'LOG',
-                'user_id' => null,
-                'commentable_type' => ProcessRequest::class,
-                'commentable_id' => $data['_request_id'],
-                'subject' => 'Email Notification Sent',
-                'body' => __(
-                    'System has sent a notification to :emails for :task',
-                    [
-                        "emails" => join(", ", $config['email']),
-                        "task" => $data['_task_name']
-                    ]
-                )
-            ]);
-        }
-
         return response()->json();
     }
-
-    private function getData($config, $data)
-    {
-        //Mustache
-        $mustache = new \Mustache_Engine;
-        if (is_array($config)) {
-            return $config;
-        }
-        $data = $mustache->render($config, $data);
-
-        if (!is_array($data)) {
-            $data = explode(',', $data);
-        }
-
-        return $data;
-    }
-
 }
--- src/Jobs/SendEmail.php
+++ src/Jobs/SendEmail.php
@@ -3,17 +3,19 @@
 namespace ProcessMaker\Packages\Connectors\Email\Jobs;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Mail\Message;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Mail\Message;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
+use ProcessMaker\Models\Comment;
 
 class SendEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $config;
     protected $properties;
 
     /**
@@ -21,9 +23,9 @@ class SendEmail implements ShouldQueue
      *
      * @param $properties
      */
-    public function __construct($properties)
+    public function __construct($config)
     {
-        $this->properties = $properties;
+        $this->config = $config;
     }
 
     /**
@@ -33,6 +35,7 @@ class SendEmail implements ShouldQueue
      */
     public function tags()
     {
+        $this->prepareEmail($this->config);
         return ['package-email', $this->properties['subject']];
     }
 
@@ -43,10 +46,110 @@ class SendEmail implements ShouldQueue
      */
     public function handle()
     {
+        $this->prepareEmail($this->config);
+
         Mail::send([], [], function (Message $message) {
             $message->to($this->properties['email'])
                 ->subject($this->properties['subject'])
                 ->setBody(view('email::layout', array_merge($this->properties, ['message' => $message]))->render(), 'text/html');
         });
+
+        if (isset($data['notification_config'])) {
+            Comment::create([
+                'type' => 'LOG',
+                'user_id' => null,
+                'commentable_type' => ProcessRequest::class,
+                'commentable_id' => $data['_request_id'],
+                'subject' => 'Email Notification Sent',
+                'body' => __(
+                    'System has sent a notification to :emails for :task',
+                    [
+                        "emails" => join(", ", $config['email']),
+                        "task" => $data['_task_name']
+                    ]
+                )
+            ]);
+        }
+    }
+
+    private function prepareEmail($config)
+    {
+        if ($this->properties) {
+            return;
+        }
+
+        //Load data
+        $data = json_decode($config['json_data'], true);
+
+        //Mustache
+        $mustache = new \Mustache_Engine;
+
+        //Mustache data notification
+        if (isset($data['notification_config'])) {
+            foreach ($data['notification_config'] as $key => $value) {
+                if (is_array($value)) {
+                    $data['notification_config'][$key] = $mustache->render(implode(", ", $value), $data);
+                } else {
+                    $data['notification_config'][$key] = $mustache->render((string) $value, $data);
+                }
+            }
+        }
+
+        //Validate data
+        $groups = !empty($config['groups']) ? $this->getData($config['groups'], $data) : [];
+        $users = !empty($config['users']) ? $this->getData($config['users'], $data) : [];
+        $additionalEmails = !empty($config['addEmails']) ? $this->getData($config['addEmails'], $data) : [];
+        $type =!empty($config['type']) ? $mustache->render($config['type'], $data) : 'screen';
+
+        //Load mails
+        $usersGroups = GroupMember::whereIn('group_id', $groups)
+            ->where('member_type', User::class)
+            ->pluck('member_id')
+            ->toArray();
+        $users = array_merge($users, $usersGroups);
+        $emails = User::whereIn('id', $users)
+            ->pluck('email')
+            ->toArray();
+
+        //Add additional emails
+        foreach ($additionalEmails as $item) {
+            $item = $mustache->render($item, $data);
+            foreach (explode(",", $item) as $email) {
+                $emails[] = trim($email);
+            }
+        }
+
+        //load Body
+        if ($type === 'screen') {
+            //screen definition
+            $screen = Screen::find($mustache->render($config['screenRef'], $data));
+            $rendered = ScreenRenderer::render($screen->config, $data);
+            $config['body'] = $rendered;
+        } else {
+            //Plain text
+            $config['body'] = htmlentities($mustache->render($config['textBody'], $data), ENT_QUOTES, 'UTF-8');
+        }
+
+        //change mustache
+        $config['subject'] = $mustache->render($config['subject'], $data);
+        $config['email'] = array_filter($emails);
+
+        $this->properties = $config;
+    }
+
+    private function getData($config, $data)
+    {
+        //Mustache
+        $mustache = new \Mustache_Engine;
+        if (is_array($config)) {
+            return $config;
+        }
+        $data = $mustache->render($config, $data);
+
+        if (!is_array($data)) {
+            $data = explode(',', $data);
+        }
+
+        return $data;
     }
 }
