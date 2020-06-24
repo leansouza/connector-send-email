7a84f625cb78bdddd0a28fd14f457e0839b08d34
sanjacornelius <sanja.cornelius@processmaker.com>
2020-06-24 11:31:18 -0700

    Add VueDeepSet

--- resources/js/ssr/src/App.vue
+++ resources/js/ssr/src/App.vue
@@ -5,12 +5,14 @@
 </template>
 
 <script>
+import * as VueDeepSet from 'vue-deepset';
 import Vue from 'vue'
 import { VueFormRenderer } from "@processmaker/screen-builder";
 import FormHtmlEditorStatic from '../../processes/screen-builder/FormHtmlEditorStatic';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
+Vue.use(VueDeepSet);
 Vue.component('FormHtmlEditorStatic', FormHtmlEditorStatic);
 Vue.component('vue-form-renderer', VueFormRenderer);
 
